739bbaf5dff02e946d8fa39f4d0e204776e23b8b
vissfesacha <sesantander@uninorte.edu.co>
2021-04-11 17:18:25 -0500

    Funcionando v2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 I  =2.3j 
 if break 1==2:
-I else
+I=
--- lab.c
+++ lab.c
@@ -8,99 +8,58 @@ extern char* yytext;
 int main(void)
 {
 	int ntoken, vtoken;
-	int array[20];
-	int z=0;
+	
 	ntoken = yylex();
-	while(ntoken) {
-		//printf("%d\n", ntoken);
-		array[z] = ntoken;
-		z=z+1;
-		ntoken = yylex();
-	}
-	//    elif in print
-
-	/* size of `array` in bytes */
-	size_t size = sizeof(array);
-
-	/* number of elements in `array` */
-	size_t length = sizeof(array) / sizeof(array[0]); 
-	
-	
-	// for (int j = 0; j < length; j++)
-	// {
-	// 	printf("Token %d\n",array[j]);
+	// while(ntoken) {
+	// 	printf("%d\n", ntoken);
+	// 	ntoken = yylex();
 	// }
-	
-		int i=0;
-	do
-	{
+	while(ntoken) {
+		printf("--------------------------\n");
+		printf("1 . El ntoken es %d\n", ntoken);
+		printf(" Contiene %s\n", yytext);
+		vtoken = yylex();
+		printf("1 . El vtoken es %d\n", vtoken);
+		while(vtoken==ESPACIO){
+			vtoken = yylex();
+		}
+		printf("2 . El vtoken es %d\n", vtoken);
+		switch (ntoken) {
+		
+		case ESPACIO:
+			printf("entro un espacio\n");
+			break;
+		
+		case IDENTIFICADOR:
 			
-				
-			
-			
-				ntoken=array[i];
-			if (ntoken != ESPACIO){
-				printf("Token %d\n", ntoken);
-				
-				vtoken = array[i+1];
-				while(vtoken==ESPACIO){
-					i=i+1;
-					vtoken = array[i+1];
-				}
-				
-				
-				
-
-				//printf(" %d Vtoken\n", vtoken);
-				
-				
-				switch (ntoken) {
-				
-				case ESPACIO:
-					break;
-				case IDENTIFICADOR:
-					
-					if(vtoken == ELSE || vtoken==RETURN || vtoken==FOR || vtoken==BREAK || vtoken==IDENTIFICADOR || vtoken==WHILE || vtoken==CONTINUE || vtoken==IF || vtoken==DEF || vtoken==IMPORT || vtoken==PASS || vtoken==PRINT || vtoken==INTEGER || vtoken==DECIMAL || vtoken==IMAGINARIO || vtoken==LONGINTEGER || vtoken==STRING){
-						printf("Identificador Syntax error\n");
-							
-					}
-					break;
-				case IMPORT:
-					if(vtoken != IDENTIFICADOR){
-						printf("Import Syntax error\n");
-					}
-					break;
-				case PASS:
-
-				case IF:
-					if(vtoken != IDENTIFICADOR && vtoken !=TRUES && vtoken !=FALSES && vtoken !=NOT && vtoken !=INTEGER && vtoken !=DECIMAL && vtoken !=IMAGINARIO && vtoken !=STRING && vtoken !=CORABRE && vtoken !=PARABRE){
-						printf("IF Syntax error\n");
-					}
-					break;
-				case ELIF:
-					
-					if(vtoken != IDENTIFICADOR && vtoken !=TRUES && vtoken !=FALSES && vtoken !=NOT && vtoken !=INTEGER && vtoken !=DECIMAL && vtoken !=IMAGINARIO && vtoken !=STRING && vtoken !=CORABRE && vtoken !=PARABRE){
-						printf("ELIF Syntax error\n");
-					}
-					break;
-			
-				default:
-					//printf("Syntax error in line %d\n",yylineno);
-					printf(" ");
-					break;   
-				}
-				
-
+			if(vtoken == ELSE || vtoken==RETURN || vtoken==FOR || vtoken==BREAK || vtoken==IDENTIFICADOR || vtoken==WHILE || vtoken==CONTINUE || vtoken==IF || vtoken==DEF || vtoken==IMPORT || vtoken==PASS || vtoken==PRINT || vtoken==INTEGER || vtoken==DECIMAL || vtoken==IMAGINARIO || vtoken==LONGINTEGER || vtoken==STRING){
+				printf("Syntax error in line %d, but found %s\n", yylineno, yytext);
+				printf("aca %d\n", vtoken);
+				 
 			}
-			i=i+1;
-			// if (array[i]<69)
-			// {
-			// 	break;
-			// }
 			
-	} while (i<length);
+			break;
+		case IF:
+            if(vtoken != IDENTIFICADOR && vtoken !=TRUES && vtoken !=FALSES && vtoken !=NOT && vtoken !=INTEGER && vtoken !=DECIMAL && vtoken !=IMAGINARIO && vtoken !=STRING && vtoken !=CORABRE && vtoken !=PARABRE){
+                printf("Syntax error in line %d, but found %s\n", yylineno, yytext);
+				printf("aca %d\n", vtoken);
+            }
+            break;
+		case ELIF:
+            if(vtoken != IDENTIFICADOR && vtoken !=TRUES && vtoken !=FALSES && vtoken !=NOT && vtoken !=INTEGER && vtoken !=DECIMAL && vtoken !=IMAGINARIO && vtoken !=STRING && vtoken !=CORABRE && vtoken !=PARABRE){
+                printf("Syntax error in line %d, but found %s\n", yylineno, yytext);
+				printf("aca %d\n", vtoken);
+            }
+            break;	
+		case IMPORT:
+            if(vtoken != IDENTIFICADOR){
+                printf("Syntax error in line %d, but found %s\n", yylineno, yytext);
+				printf("aca %d\n", vtoken);
+            }
+		}
+		ntoken = vtoken;
+		printf("2 . El ntoken es %d\n", ntoken);
+	}
 
 	return 0;
 }
-
-
