b15781f8f3053c0681123cc0bc54902d30874468
vissfesacha <sesantander@uninorte.edu.co>
2021-04-12 11:32:17 -0500

    TERMINADO FUNCIONANDO

--- program.py
+++ program.py
@@ -1,51 +1,51 @@
-# def eval(xi, exp):
-# 2ans2 = False
-# for i in range(len(exp)) :
-# part =? True
-# for j in range(len(exp[i])) :
-# part = part and xi[exp[i][j]]
-# ans = ans or part
-# return ans
-# # comentario ignorado
-# while 2*i<=40000L
-# h = 3.4e-5 + 5j
-# if i == 0:
-# break
-# def otra_fun():
-# pass
-# print("Esta es la respuesta",con, "y la expresión ", 3*i+5*(7-x))
-# #string de la hipotesis
-# table = []
-# y = []
-# #i = 0
-# 123ABC
-import random 
-def miller_rabin(n, k):
+def eval(xi, exp):
+2ans2 = False
+for i in range(len(exp)) :
+part =? True
+for j in range(len(exp[i])) :
+part = part and xi[exp[i][j]]
+ans = ans or part
+return ans
+# comentario ignorado
+while 2*i<=40000L
+h = 3.4e-5 + 5j
+if i == 0:
+break
+def otra_fun():
+pass
+print("Esta es la respuesta",con, "y la expresión ", 3*i+5*(7-x))
+#string de la hipotesis
+table = []
+y = []
+#i = 0
+123ABC
+# import random 
+# def miller_rabin(n, k):
     
-    if n == 2:
-        return True
+#     if n == 2:
+#         return True
 
-    if n % 2 == 0:
-        return False
+#     if n % 2 == 0:
+#         return False
 
-    r, s = 0, n - 1
-    while s % 2 == 0:
-        r += 1
-        s //= 2
-    for _ in range(k):
-        a =  random.randrange(2, n - 1)
-        x = pow(a, s, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
+#     r, s = 0, n - 1
+#     while s % 2 == 0:
+#         r += 1
+#         s //= 2
+#     for _ in range(k):
+#         a =  random.randrange(2, n - 1)
+#         x = pow(a, s, n)
+#         if x == 1 or x == n - 1:
+#             continue
+#         for _ in range(r - 1):
+#             x = pow(x, 2, n)
+#             if x == n - 1:
+#                 break
+#         else:
+#             return False
+#     return True
 
-print(miller_rabin(n,k))
+# print(miller_rabin(n,k))
 
 
 # CODIGO SACADO DE https://gist.github.com/Ayrx/5884790
